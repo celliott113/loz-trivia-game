705c58086af97d5e38d72b8836aa515276af7ef1
Christopher Elliott <christopher.elliott8@gmail.com>
2017-08-26 21:23:47 -0500

    Commit #11 - Update to answer timings

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -68,7 +68,7 @@ function generateLoss() {
     gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/wrong_answer.gif'>";
     $(".mainArea").html(gameHTML);
     wrongChoice.play();
-    setTimeout(wait, 6000);
+    setTimeout(wait, 3000);
 }
 
 //==================== A function to generateHTML for the countdown timer & multiple choice placement ====================//
@@ -107,7 +107,7 @@ function timerWrapper() {
 
 //==================== A function to display quiz results & place a reset game button ====================//
 function finalScreen() {
-    gameHTML = "<p class='text-center triforceFont2 timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-danger btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+    gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-danger btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
     $(".mainArea").html(gameHTML);
 }
 
